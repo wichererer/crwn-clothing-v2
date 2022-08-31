add38b2f97670d3ed01427e1d96dcdda8f758501
Paweł Wichrowski <pawel.wichrowski@vig-polska.pl>
2022-08-31 16:34:52 +0200

    stripe

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import {PersistGate} from 'redux-persist/integration/react';
+import {Elements} from "@stripe/react-stripe-js";
 
 import App from './App';
 
 import './index.scss';
 import { persistor, store } from './store/store';
+import {stripePromise} from "./utils/stripe/stripe.utils";
 
 const rootElement = document.getElementById('root');
 
@@ -16,7 +18,10 @@ render(
         <Provider store={store}>
             <PersistGate loading={null} persistor={persistor}>
             <BrowserRouter>
+                <Elements stripe={stripePromise}>
+
                 <App/>
+                </Elements>
             </BrowserRouter>
             </PersistGate>
         </Provider>
